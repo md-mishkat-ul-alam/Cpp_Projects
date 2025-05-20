ea8ba564e39538171f0304d08fef9eaeec73ac16
md-mishkat-ul-alam <mishkatulalam30@gmail.com>
2025-05-21 02:12:29 +0600

    adding practise file

---
+++ 8. Accept user input.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+// cout << (insertion operator)
+// cin >> (extraction operator)
+
+int main() 
+{
+   std::string name;
+   int age;
+   char string[50];
+
+   std::cout << "What's your full name?: ";
+   std::getline(std::cin >> std::ws, name);
+   std::cin.get(string,50);
+
+   std::cout << "What's your age?: ";
+   std::cin >> age;
+
+   std::cout << "Hello " << name << '\n';
+   std::cout << "You are " << age << " years old";
+
+   return 0;
+}
